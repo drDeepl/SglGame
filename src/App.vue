b58d375f513c9273190bfc87f8406aaf60bd991b
Dr.Phen <Vlimaster@yandex.ru>
2023-02-22 10:14:43 +0700

    fixed account btn

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template id="app">
-  <va-navbar color="primary">
+  <va-navbar color="secondary pa-0">
     <template #left>
-      <v-navbar-item>Logo</v-navbar-item>
+      <v-navbar-item>SQL Game</v-navbar-item>
     </template>
 
     <template #right>
-      <va-button><va-avatar color="danger">?</va-avatar></va-button>
+      <va-button round size="small"
+        ><va-avatar color="danger">?</va-avatar></va-button
+      >
       <v-navbar-item>Account</v-navbar-item>
     </template>
   </va-navbar>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
